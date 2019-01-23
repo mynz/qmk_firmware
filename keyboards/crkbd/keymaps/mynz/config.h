6925f55f060a14c1c8f34b1d84ba5a5140c0d0f0
mynz <rollingteapot@gmail.com>
2019-01-24 01:42:38 +0900

    RETRO_TAPPING を有効化

--- keyboards/crkbd/keymaps/mynz/config.h
+++ keyboards/crkbd/keymaps/mynz/config.h
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+//#define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #define SSD1306OLED
@@ -34,6 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
+
+#define RETRO_TAPPING
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
