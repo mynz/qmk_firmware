abed18ee7cf04a8aa477c1a805e2a756374fe4b3
morimoto_atsushi <morimoto_atsushi@cygames.co.jp>
2019-02-12 11:34:44 +0900

    リーダーキーを有効化

--- keyboards/crkbd/keymaps/mynz_mix/rules.mk
+++ keyboards/crkbd/keymaps/mynz_mix/rules.mk
@@ -17,6 +17,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 TAP_DANCE_ENABLE = yes      # タップダンスの有効化
+LEADER_ENABLE = yes         # リーダーキーの有効化
 FOR_MAC = no                # Mac向けの設定
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
